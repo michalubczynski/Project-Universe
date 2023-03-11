097334b25014b4fbac0c66575cd878528efe5fa4
andrzagorski <72298491+andrzagorski@users.noreply.github.com>
2023-03-11 20:29:30 +0100

    blank project

---
+++ Universe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Universe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Universe/Models/Galaxy.cs
@@ -0,0 +1,11 @@
+﻿namespace Universe.Models
+{
+    public class Galaxy
+    {
+        public int GalaxyId { get; set; }
+        public string Name { get; set; }
+       // public enum type { get; set; } // enum
+        public double mass { get; set; }
+
+    }
+}
