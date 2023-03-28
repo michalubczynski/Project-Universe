9a136070b3de5971a635514a7655713e01c65700
michalubczynski <michlub322@student.polsl.pl>
2023-03-28 14:58:54 +0200

    UnitOfWork and New Repository<T>

---
+++ Universe/Models/Repository.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Universe.Models.starsystem;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Universe.Models
+{
+    public class Repository<T> : IRepository<T>, IDisposable where T : class
+    {
+        private DbUniverse context;
+
+        public Repository(DbUniverse context)
+        {
+            this.context = context;
+        }
+
+        private bool disposed = false;
+
+        public void Delete(int deletedId)
+        {
+            throw new NotImplementedException();
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+                disposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public T GetByID(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetList()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T inserted)
+        {
+
+            context.Set<T>().Add(inserted);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void Update(T updated)
+        {
+            context.Entry(updated).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+
+        }
+    }
+}
---
+++ Universe/Models/UnitOfWork.cs
@@ -0,0 +1,65 @@
+﻿using Universe.Models.discoverer;
+using Universe.Models.galaxy;
+using Universe.Models.planet;
+using Universe.Models.ship;
+using Universe.Models.star;
+using Universe.Models.starsystem;
+
+namespace Universe.Models
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbUniverse _context;
+        private Dictionary<Type, object> _repositories;
+
+
+        public UnitOfWork(DbUniverse context)
+        {
+            _context = context;
+            _repositories = new Dictionary<Type, object>();
+        }
+
+        private bool _disposed = false;
+
+        public IRepository<T> GetRepository<T>() where T : class
+        {
+            if (_repositories.TryGetValue(typeof(T), out var repository))
+            {
+                return (IRepository<T>)repository;
+            }
+
+            var newRepository = new Repository<T>(_context);
+            _repositories.Add(typeof(T), newRepository);
+
+            return newRepository;
+
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public Task SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
