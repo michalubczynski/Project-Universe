0df3c62399c2a74d42a9a9109647c4b37e815e24
andrzagorski <72298491+andrzagorski@users.noreply.github.com>
2023-04-11 17:10:17 +0200

    BLL

---
+++ BuisnessLogicLayer/AdministractionActions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Universe.Models;
+
+namespace BuisnessLogicLayer
+{
+    // Interfejs dla klasy Administracja
+    public interface IAdministrationOperation
+    {
+        public bool CreateDiscoverer();
+        public bool KillDiscoverer();
+        public bool CreateGalaxy();
+        public bool MigrateSolarSystem();
+    }
+
+    internal class AdministractionActions:IAdministrationOperation
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AdministractionActions(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public bool CreateDiscoverer()
+        {
+            return false;
+        }
+        public bool KillDiscoverer() {  return false; } 
+        public bool MigrateGalaxy() { return false; }
+        public bool CreateGalaxy() { return false; }
+        public bool MigrateSolarSystem() { return false; }
+
+
+    }
+}
---
+++ BuisnessLogicLayer/DiscovererActions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Universe.Models;
+
+namespace BuisnessLogicLayer
+{
+    public interface IDiscovererOperation
+    {
+        public bool DiscoverPlanet();
+        public bool BuyNewShip();
+    }
+
+    internal class DiscovererActions:IDiscovererOperation
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public DiscovererActions(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public bool DiscoverPlanet()
+        {
+            return false;
+        }
+
+        public bool BuyNewShip()
+        {
+            return false;
+        }
+
+    }
+}
