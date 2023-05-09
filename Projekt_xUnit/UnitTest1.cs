5e8739fec803fed79c0af3b3c0b72f5d2290e8de
Artur Kuś <88733904+artqs01@users.noreply.github.com>
2023-05-09 04:12:45 -0700

    in memory db not working

--- Projekt_xUnit/UnitTest1.cs
+++ Projekt_xUnit/UnitTest1.cs
@@ -1,17 +1,20 @@
 using BLL_BuisnessLogicLayer;
 using BuisnessLogicLayer;
 using DAL_DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using Models;
 using System.Numerics;
 using Universe.Models;
 using Universe.Models.galaxy;
 using Universe.Models.planet;
+using Universe.Models.star;
 
 namespace Projekt_xUnit
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void BLLTest()
         {
 
         var planets = new List<Planet>{
@@ -34,7 +37,7 @@ namespace Projekt_xUnit
             Assert.Equal(planets.ElementAt(2).Id, result.Result.Id);
         }
         [Fact]
-        public void Test2()
+        public void MockTest()
         {
             var mockRepo = new MockEntityRepository<Planet>().MockCountAsync(3).MockGetByID(new Planet { Id = 1, Name = "Planet A", Mass = 10 });
             var unitOfWork = new UnitOfWork();
@@ -42,6 +45,36 @@ namespace Projekt_xUnit
             var service = new Service(unitOfWork);
             var result = service.GetAllPlanetsCount();
             Assert.Equal(3, result.Result);
+        }
+        public static DbUniverse GetTestDbContext(string dbName)
+        {
+            // Create db context options specifying in memory database
+            var options = new DbContextOptionsBuilder<DbUniverse>()
+            .UseInMemoryDatabase(databaseName: dbName)
+            .Options;
+
+            //Use this to instantiate the db context
+            return new DbUniverse(options);
+
+        }
+
+        private DbUniverse GetTestDatabase()
+        {
+            var testContext = GetTestDbContext(nameof(AddRandomStarsTest));
+            return testContext;
+        }
+
+        [Fact]
+        public void AddRandomStarsTest()
+        {
+            var testContext = GetTestDatabase();
+            var starsRepo = new Repository<Star>(testContext);
+            var unitOfWork = new TestUnitOfWork();
+            unitOfWork.AddRepository(starsRepo);
+            var service = new Service(unitOfWork);
+            var count = 1;
+            var result = service.AddRandomStars(count);
+            Assert.Equal(count, unitOfWork.GetRepository<Star>().GetList().Count());
         }
     }
 }
