8361684023a18e0b4871300ee1e4e39d12024b03
michalubczynski <michlub322@student.polsl.pl>
2023-05-09 22:28:18 +0200

    Removed unused services and rename test

---
+++ Projekt_xUnit/TestsV1.cs
@@ -0,0 +1,93 @@
+using BLL_BuisnessLogicLayer;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Moq;
+using Tests_xUnit.FakeClasses;
+using Universe.Models;
+using Universe.Models.planet;
+using Universe.Models.star;
+
+namespace Projekt_xUnit
+{
+	public class TestsV1
+	{
+		[Fact]
+		public void BLLTest()
+		{
+
+		var planets = new List<Planet>{
+			new Planet { Id = 1, Name = "Planet A", Mass = 10 },
+			new Planet { Id = 2, Name = "Planet B", Mass = 5 },
+			new Planet { Id = 3, Name = "Planet C", Mass = 15 }
+		};
+
+			var fakeRepository = new FakeRepository<Planet>();
+
+			foreach (var p in planets)
+			{
+				fakeRepository.Insert(p);
+			}
+
+			TestUnitOfWork unitOfWork = new TestUnitOfWork();
+			unitOfWork.AddRepository(fakeRepository);
+			var service = new Service(unitOfWork);
+			var result = service.GetHeaviestPlanet();
+			Assert.Equal(planets.ElementAt(2).Id, result.Result.Id);
+		}
+		[Fact]
+		public void MockTest()
+		{
+			var mockRepo = new Mock<IRepository<Planet>>();
+			var p1 = new Planet
+			{
+				Id = 0,
+				Name = "aaa"
+			};
+			var p2 = new Planet
+			{
+				Id = 1,
+				Name = "bbb"
+			};
+			mockRepo.Setup(x => x.GetListAsync().Result).Returns(new List<Planet> {p1, p2});
+			var unitOfWork = new UnitOfWork();
+			unitOfWork.AddRepository(mockRepo.Object);
+			var service = new Service(unitOfWork);
+			var result = service.GetAllPlanetsCount();
+			Assert.Equal(2, result.Result);
+		}
+		public static DbUniverse GetTestDbContext(string dbName)
+		{
+			// Create db context options specifying in memory database
+			var options = new DbContextOptionsBuilder<DbUniverse>()
+			.UseInMemoryDatabase(databaseName: dbName)
+			.Options;
+
+			//Use this to instantiate the db context
+			return new DbUniverse(options);
+
+		}
+		private DbUniverse GetTestDatabase()
+		{
+			var testContext = GetTestDbContext(nameof(AddRandomStarsTest));
+			return testContext;
+		}
+		[Fact]
+		public void AddRandomStarsTest()
+		{
+			var testContext = GetTestDatabase();
+			var starsRepo = new Repository<Star>(testContext);
+			var unitOfWork = new UnitOfWork();
+			unitOfWork.AddRepository(starsRepo);
+			var service = new Service(unitOfWork);
+			var count = 2;
+			var result = service.AddRandomStars(count);
+			Assert.Equal(count, unitOfWork.GetRepository<Star>().GetList().Count());
+		}
+		[Fact]
+		public void DummyPlanetRepoTest()
+		{
+			Action dummyActCreate = () => new Repository<Planet>(null);
+			Assert.Throws<NullReferenceException>(dummyActCreate);
+		}
+	}
+}
